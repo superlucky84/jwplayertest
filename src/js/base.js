b78ed3f7077bc6e064f2763804a1fddbac246c6b
kimjinwoo <superlucky84@gmail.com>
2017-09-17 16:48:17 +0900

    * FIXED ESLINT

--- src/js/base.js
+++ src/js/base.js
@@ -56,8 +56,12 @@ var EventUtil = {
       target.className = origClassName.replace(re,"");
     }
   }
-
 };
+
+
+/**
+ * PLOYFILL
+ */
 if (typeof Object.assign != 'function') {
   (function () {
     Object.assign = function (target) {
@@ -168,11 +172,6 @@ String.prototype.matchAll = function(regexp) {
   return matches.length ? matches : null;
 };
 
-function inherit(subType, superType) {
-  var prototype = object(superType.prototype);
-  prototype.constructor = subType;
-}
-
 function template (template, map) { 
 	var template_new = template; 
   if (template.matchAll(/{\$([^}]+)}/g)) { 
@@ -182,6 +181,7 @@ function template (template, map) {
   } 
   return template_new;
 }
+
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -200,8 +200,6 @@ function shuffle(array) {
 
   return array;
 }
-
-
 
 var filenamelist=["Take Five", "All Of Me (MBC `우리 결혼했어요` 삽입곡)", "It`s a Sin To tell a Lie", "Straight No Chaser", "Lullaby of Birdland", "Absolutely", "Bach-Ing Mad", "Take a Letter", "Steps Behind", "Oleo", "No It Ain't", "Canteloupe Island", "Lover Come Back To Me", "Stardust", "Lullaby of Birdland", "In The Mood", "How High the Moon", "The Sex Pest", "Footprints", "Startin' out Again", "The Jester", "Chocolate", "Fly Me To The Moon", "I Tried to Tell You", "Waltz for Joshua", "Black Orpheus", "Your Song", "That's What You Get", "Knuckle Shuffle", "Monkey Jazz", "Harry Hates Ham", "Chill Baby", "Do It Anyway", "Freddie Freeloader", "Maiden Voyage", "Bossa Scousa", "Think It Over", "Cheeky", "Caravan", "Cha Cha Charlie", "Oh When The Saints Go Marching In", "Summertime", "Why Don`t You Do Right", "Jazz", "Minor Swing", "Love Me Or Leave Me", "A Taste Of Honey", "Georgia On My Mind", "It`s Too Late", "Stardust", "Antonio`s Song (The rainbow)", "Jazz", "Chattanooga Choo Choo", "Moon Light Serenade", "My Man", "Saint Louis Blues", "Take The A Train", "Nuages", "Taking A Chance On Love", "The Scat Song", "You Rascal You", "I Got Rhythm", "Round Midnight (Live)", "Ain`t Misbehavin`", "The Calypso", "Jazz (We've Got)", "Oo-Wee Walkie Talkie", "Waltz For Debby", "Love Letter", "Jazz-Suite No. 2: Lyric Waltz. Allegretto", "Jazz", "Jazz (We've Got)", "All Or Nothing At All", "Jazz-Suite Nr.1: Waltz", "Jazz", "Love Again (Feat. Jill Scott)", "Jazz (New Jazz Ver.)", "Jazz-Bar", "Fare Thee Well; Annabelle", "Jazz!", "Jazz 01", "빈 바다", "Growing Grooves (Marimba Chill Out Mix)", "The Battle Hymn of the Republic / When Johhny Comes...", "Mr. Wonder", "Hot Piano", "When I Fall In Love", "Jazz 07", "Jazz 16", "Jazz 05", "Jazz 02", "Jazz 18", "Jazz 09", "Jazz 03", "Jazz 06", "Jazz 10", "Jazz 04", "Jazz 08", "Jazz 11", "Jazz 17", "Jazz 14", "Jazz 12", "Don't Get Around Much Anymore", "Lizard Skin", "Wishing On a Star", "Jazz-Suite No. 2: Finale. Allegro moderato", "Jazz 13", "Jazz 15", "I`m A Fool To Want You", "Lover", "Night & Day", "Jazz-Suite No. 2: Waltz I", "Renacido", "Jazz-Suite No. 2: Dance I. Presto", "Jazz-Suite No. 2: Waltz II. Allegretto poco moderato", "Jazz-Suite No. 2: Marcia", "Jazz-Suite No. 2: Dance II. Allegretto scherzando", "Jazz-Suite No. 2: Little Polka. Allegretto", "아리랑 (구전 민요)", "You Do Something To Me", "Closing Time Blues", "Jazz (Ain`t Nothin` But Soul)", "The Duke", "I'm Thru With Love", "한오백년 (구전 민요)", "해맑은 아침 (Softly, As In A Morning Sunrise)", "Sometimes I'm happy", "Serious", "Indiana", "I Can't Believe That You're In Love With Me", "Let'S Face The Music And Dance", "I Let A Song Go Out Of My Heart", "Solitude", "If It's a Game", "The Things We Did Last Summer", "Here's to Horace", "Love Walked In", "Round Midnight", "가시리", "Jazz", "When Lights Are Low", "Splanky", "Li'l Darlin", "Den Lyssnande Maria", "Come Sunday", "Strawberry Fields Forever", "I've Found A New Baby", "Good Bait", "There Will Never Be Another You", "Jazz (We've Got) (Re-Recording)"];
 
